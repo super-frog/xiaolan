a587a87a90fff472a61604cdbcaf0b263d83bafc
lanhao <admin@lanhao.name>
2015-10-21 15:08:16 +0800

    redis

--- tpl/config/config.js
+++ tpl/config/config.js
@@ -2,27 +2,39 @@
  * Created by lanhao on 15/5/17.
  */
 module.exports = {
-    'version':'1.0.0',
-    'port':3001,
-    'ip':null,
-    'cors':false,
-    'static':['js','css','jpg','png','ico'],
-    'modules':{
-        'mysql':{
-            'import':1,
-            'config':{
-                "host":"127.0.0.1",
-                "port":"3306",
-                "user":"root",
-                "password":"",
-                "database":"vdb_1"
-            }
-        },
-        'session':{
-            'import':1,
-            'config':{
-                'age':60
-            }
-        }
+  'version': '1.0.0',
+  'port': 3001,
+  'ip': null,
+  'cors': false,
+  'static': ['js', 'css', 'jpg', 'png', 'ico'],
+  'modules': {
+    'mysql': {
+      'import': 1,
+      'config': {
+        "host": "127.0.0.1",
+        "port": "3306",
+        "user": "root",
+        "password": "",
+        "database": "vdb_1"
+      }
+    },
+    'session': {
+      'import': 1,
+      'config': {
+        'age': 60
+      }
     }
+    ,
+    'tools': {
+      'import': 1,
+      'config': {}
+    },
+    'redis':{
+      'import':1,
+      'config':{
+        'port':6379,
+        'host':'127.0.0.1'
+      }
+    }
+  }
 };
