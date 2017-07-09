0f40d11331f0c63ee422c7fc879c19ceb1d87cda
captainblue2013 <admin@lanhao.name>
2017-07-09 16:25:42 +0800

    .*

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
 "use strict";
 
 
-var http = require('http');
+const http = require('http');
 const EOL = require('os').EOL;
+const fs = require('fs');
 
-var Xiaolan = {};
+let Xiaolan = {};
 
 Xiaolan.basePath = process.cwd();
 
@@ -21,7 +22,7 @@ Xiaolan.init = function (config) {
 
   if (config.modules) {
     Xiaolan.libs = {};
-    for (var k in config.modules) {
+    for (let k in config.modules) {
       if (config.modules[k].import) {
         Xiaolan.libs[k] = require('./lib/' + k)(config.modules[k].config);
         console.log('Load [' + k + ']  ok!');
@@ -34,20 +35,24 @@ Xiaolan.init = function (config) {
 Xiaolan.route = null;
 
 Xiaolan.register = function (map) {
-  var Route = require('./lib/route');
+  let Route = require('./lib/route');
   Route.register(map);
   Xiaolan.route = Route.routingTable;
 };
 
 Xiaolan.createServer = function () {
-  Xiaolan.register(require(process.cwd() + '/routes'));
-  var Request = require('./lib/request');
-  var Response = require('./lib/response');
-  var serveStatic = require('serve-static');
-  var serve = serveStatic(process.cwd() + '/views/');
-  var config = global.app.config;
-  var _port = config.port ? config.port : 3000;
-  var _ip = config.ip ? config.ip : null;
+  if(fs.existsSync(process.cwd() + '/routes.js')) {
+    Xiaolan.register(require(process.cwd() + '/routes'));
+  }else{
+    Xiaolan.register({});
+  }
+  let Request = require('./lib/request');
+  let Response = require('./lib/response');
+  let serveStatic = require('serve-static');
+  let serve = serveStatic(process.cwd() + '/views/');
+  let config = global.app.config;
+  let _port = config.port ? config.port : 3000;
+  let _ip = config.ip ? config.ip : null;
 
   http.createServer(function (req, res) {
     let _date = req.start = res.start = new Date();
@@ -89,9 +94,9 @@ Xiaolan.handler = function (req, res) {
   if (Object.keys(Xiaolan.route).length > 0) {
     let method = req.method.toLocaleLowerCase();
     let uri = req.pathInfo;
-    var matched = false;
+    let matched = false;
     if (Xiaolan.route[method]) {
-      for (var k in Xiaolan.route[method]) {
+      for (let k in Xiaolan.route[method]) {
         if (Xiaolan.route[method][k].reg.test(uri)) {
           matched = true;
           break;
@@ -106,6 +111,7 @@ Xiaolan.handler = function (req, res) {
       res.end('404 not found');
     }
   } else {
+
     try {
       require(Xiaolan.basePath + '/controllers/' + req.params[0])[req.params[1]](req, res);
     } catch (ex) {
