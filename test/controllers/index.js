9a5bdb06fa70ccf46b69355f13676e8e56d6a760
captainblue2013 <admin@lanhao.name>
2017-08-14 17:30:52 +0800

    memory storage

--- test/controllers/index.js
+++ test/controllers/index.js
@@ -6,12 +6,13 @@
 
 var Controller = {};
 
-Controller.index = function (req,res) {
-    res.json(200,req.session);
+Controller.index = function (req, res) {
+  req.session['count'] = req.session['count']+1 || 1;
+  res.json(200, req.session);
 };
 
-Controller.detail = function(req,res){
-    res.end('detail');
+Controller.detail = function (req, res) {
+  res.end('detail');
 }
 
 module.exports = Controller;
