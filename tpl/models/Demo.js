7ecaea00f1041f1d95e38409bb4bbb5611660bef
captainblue2013 <admin@lanhao.name>
2018-03-20 11:19:37 +0800

    fixed bug

--- tpl/models/Demo.js
+++ tpl/models/Demo.js
@@ -3,10 +3,12 @@
  */
 
 'use strict';
-const {Field, Table, Migrate} = require('xiaolan-db');
+const {Field, Table, Migrate, Presets} = require('xiaolan-db');
 
 module.exports = new Table('my_user', {
-  id:Field.name('user_id').bigint(true).primary().AI().comment('p id'),
+  ...Presets.AI,
   name: Field.name("user_name").varchar(64).allowNull().default("foo").uniq('u_a').comment("name of user"),
-  age: Field.name("user_age").tinyint().default(10).comment('ages').index()
+  age: Field.name("user_age").tinyint().default(10).comment('ages').index(),
+  ...Presets.opTime,
+
 });
